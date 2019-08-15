66ae10b5896a5aa0429408c52ab189bf0ba3ffa7
exosite-winnie <winnieli@exosite.com>
2019-08-15 15:20:19 +0800

    1

--- endpoints/example.lua
+++ endpoints/example.lua
@@ -1,5 +1,5 @@
 --#ENDPOINT POST /lightbulb/{sn}
-print("Creating a new user3")
+print("Creating a new user4")
 
 --#ENDPOINT GET /api/user/{userId}
 print("Fetch a given user" .. request.parameters.userId)
